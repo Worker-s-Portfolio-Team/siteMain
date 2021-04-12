4f9999f23c61a60b44606ab1dca40be4f33fdd24
daservajesus <16702642+daservajesus@users.noreply.github.com>
2021-04-12 19:35:52 +0100

    Temporarily comment JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+    /*
     $.ajax({
         url: "/",
         type: 'GET',
         dataType: 'json',
         success: function(data) {
-            // $("#request").html(JSON.stringify(data, null, 2));
-            // let ip_address      =   data.headers["cf-connecting-ip"];
+
         }
     });
+    */
 });
